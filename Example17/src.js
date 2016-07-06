896c6b054466123eee2e9134d00589af8066bdfe
Dmurkay <dbkai@ya.ru>
2016-07-06 17:11:21 +0500

    exercise 17

---
+++ Example17/src.js
@@ -0,0 +1,25 @@
+//babel --presets react src.js -o dist.js --watch
+"use strict"
+
+const App = () => {
+  return (
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <th>heading</th>
+          <th>heading</th>
+          <th>heading</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr id="theRow">
+          <td>
+            <form action=""></form>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
